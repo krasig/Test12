f49ff0b20593cf0e8a0b3d21b728ad3e46bb85d3
krasi <krasi@krasi-Precision-3510>
2018-10-29 14:46:40 +0200

    fff

--- PDoi/src/main/java/delme/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iisda.government.bg/RAS/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package delme;
--- PDoi/src/main/java/delme2/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ssp.indexbg.bg/")
-package delme2;
